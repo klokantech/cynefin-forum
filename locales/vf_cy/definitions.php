a52fd078adc2cf0e302c768b92aee8ea9843f8cf
Dalibor Janák <dalibor.janak@gmail.com>
2016-03-14 18:41:43 +0100

    Welch locales

---
+++ locales/vf_cy/definitions.php
@@ -0,0 +1,11 @@
+<?php
+
+ $LocaleInfo['vf_cy'] = array (
+  'Locale' => 'cy',
+  'Name' => 'Cymraeg',
+  'Description' => 'Welch language translations for Vanilla.',
+  'Version' => '2016.01.26p1700',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/cy/',
+  'License' => 'GPLv2',
+);
